378c458ccc0c2c99a3d152f21444203311d94593
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-10-30 16:23:33 +0100

    challenges

---
+++ task2/task2.js
@@ -0,0 +1,9 @@
+
+function arrayTriplets(array) {
+    const sortedArray = array.sort();
+    for (i = 0; i < sortedArray.length; i++) {
+        if (array[i] === 3) {
+            for (i = 0; i < sortedArray.length; i++)
+        }
+    }
+}
